2c4cf78ec4c6785fac6484b9e4b6a70c912776d8
i1number1 <zhigalkini007@gmail.com>
2025-12-17 01:00:28 +0300

    Добавил 2 теста: "Добавление нового питомца" и "Добавление нового питомца с полными данными"

--- Tests/test_pet.py
+++ Tests/test_pet.py
@@ -1,13 +1,17 @@
 import allure
-import  requests
+import jsonschema
+import requests
+from .schemas.pet_schema import PET_SCHEMA
+
 BASE_URL = "http://5.181.109.28:9090/api/v3"
+
 
 @allure.feature("Pet")
 class TestPet:
 
+
     @allure.title("Попытка удалить несуществующего питомца")
     def test_delete_nonexistent_pet(self):
-
         with allure.step("Отправка запроса на удаление несуществующего питомца"):
             response = requests.delete(url=f"{BASE_URL}/pet/9999")
 
@@ -20,9 +24,7 @@ class TestPet:
 
     @allure.title("Попытка обновить несуществующего питомца")
     def test_update_nonexistent_pet(self):
-
         with allure.step("Отправка запроса на обновление несуществующего питомца"):
-
             payload = {
                 "id": 9999,
                 "name": "Non-existent Pet",
@@ -49,3 +51,62 @@ class TestPet:
 
         with allure.step("Проверка текстового содержимого ответа"):
             assert response.text == "Pet not found", "Текст ошибки не совпал с ожидаемым"
+
+
+
+    @allure.title("Добавление нового питомца")
+    def test_add_pet(self):
+
+        with allure.step("Подготовка данных для создания питомца"):
+            payload = {
+                "id": 1,
+                "name": "Buddy",
+                "status": "available"
+            }
+
+        with allure.step("Отправка запроса на создание питомца"):
+            response = requests.post(url=f"{BASE_URL}/pet", json=payload)
+
+        response_json = response.json()  ## Добавлена переменная, так как это проще, чем вызывать функцию несколько раз в assert
+
+        with allure.step("Проверка статуса ответа и валидация JSON-схемы"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+            jsonschema.validate(response.json(), PET_SCHEMA)
+
+        with allure.step("Проверка параметров питомца в ответе"):
+            assert response_json['id'] == payload['id'], "id питомца не совпадает с ожидаемым"
+            assert response_json['name'] == payload['name'], "Имя питомца не совпадает с ожидаемым"
+            assert response_json['status'] == payload['status'], "Статус питомца не совпадает с ожидаемым"
+
+    @allure.title("Добавление нового питомца с полными данными")
+    def test_full_add_pet(self):
+
+        with allure.step("Подготовка данных для создания питомца"):
+            payload = {
+                "id": 10,
+                "name": "doggie",
+                "category": {
+                    "id": 1,
+                    "name": "Dogs"
+                },
+                "photoUrls": ["string"],
+                "tags": [{"id": 0, "name": "string"}],
+                "status": "available"
+            }
+
+        with allure.step("Отправка запроса на создание питомца"):
+            response = requests.post(url=f"{BASE_URL}/pet", json=payload)
+
+        response_json = response.json()  ## Добавлена переменная, так как это проще, чем вызывать функцию несколько раз в assert
+
+        with allure.step("Проверка статуса ответа и валидация JSON-схемы"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+            jsonschema.validate(response.json(), PET_SCHEMA)
+
+        with allure.step("Проверка параметров питомца в ответе"):
+            assert response_json['id'] == payload['id'], "id питомца не совпадает с ожидаемым"
+            assert response_json['name'] == payload['name'], "Имя питомца не совпадает с ожидаемым"
+            assert response_json['category'] == payload['category'], "Категория питомца не совпадает с ожидаемым"
+            assert response_json['photoUrls'] == payload['photoUrls'], "ФотоURLs питомца не совпадает с ожидаемым"
+            assert response_json['tags'] == payload['tags'], "Статус питомца не совпадает с ожидаемым"
+            assert response_json['status'] == payload['status'], "Статус питомца не совпадает с ожидаемым"
